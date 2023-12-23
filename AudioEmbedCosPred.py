599b48d1c7a476aaae9b36d1a03baf23e10e0723
flokithetraveler <75645468+flokithetraveler@users.noreply.github.com>
2023-12-23 22:16:31 +0100

    Update AudioEmbedSimCosPred.py

--- AudioEmbedCosPred.py
+++ AudioEmbedCosPred.py
@@ -1,50 +1,23 @@
 #Facebook's Wav2Vec2
-
 #The base model pretrained and fine-tuned on 960 hours of Librispeech on 16kHz sampled speech audio.
 #When using the model make sure that your speech input is also sampled at 16Khz.
 
 import torch
-from scipy import spatial
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, pipeline
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import speech_recognition as sr
 import io
 from pydub import AudioSegment
-import numpy
-from numpy import dot
-from numpy.linalg import norm
-from datasets import load_dataset
-from sklearn.model_selection import train_test_split
+import numpy as np
 
 tokenizer = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
 model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-
-# --------------- train model with added noise ----------------------------------------------------------
-#dataset = load_dataset('LJSpeech-1.1/LJSpeech-1.1/wavs')['train']
-#dataset.shuffle()
-#noise = np.random.normal(-0.5*min(dataset[i]['audio']['array']),0.5*max(dataset[i]['audio']['array']),len(dataset[i]['audio']['array']))
-
-#tokenized_datasets = tokenizer([dataset['train'][i]['audio']['array'] for i in range(1000)])
-#tensors_audios = [torch.FloatTensor(dataset[i]['audio']['array']) for i in range(0,1000)]
-
-#X_train, X_valid, y_train, y_valid = train_test_split(tokenized_datasets, full_y_train, test_size=0.1, random_state=42)
-
-#vectorizer = CountVectorizer(lowercase=True)
-#full_X_train = vectorizer.fit_transform(training_corpus.data)
-#full_y_train = training_corpus.target
-
-# On le divise en données d'entraînement et de validation
-#X_train, X_valid, y_train, y_valid = train_test_split(full_X_train, full_y_train, test_size=0.1, random_state=42)
-# ------------------------------------------- training -------------------------------------------------
-
-#EleutherAI = 'EleutherAI/gpt-neo-125M'
-#fb="facebook/opt-350m"
-#generator = pipeline('text-generation', model=fb)
 
 r = sr.Recognizer()
 m = sr.Microphone(sample_rate=16000)
 
 number_of_words_to_ban = int(input("Enter the number of words you want to ban : "))
 banned_words_tensors = []
+banned_words_str = []
 
 while number_of_words_to_ban != 0:
     print("Please say out loud the word that you want to ban")
@@ -60,31 +33,68 @@ while number_of_words_to_ban != 0:
 
         if words != '':
             print('You said : ', words, 'is this correct ?')
-            res = input("Yes(y) No(n) : ")
+            res = input("Yes(y)/No(n) : ")
             if res == 'y':
-                mean = numpy.mean(tokens[0].numpy())
-                #var = numpy.var(tokens[0].numpy())
-                #banned_words_tensors.append((tokens[0].numpy()+mean)/var)
-                banned_words_tensors.append(tokens[0].numpy()+mean)
+                mean = np.mean(tokens[0].numpy())
+                #banned_words_tensors.append(tokens[0].numpy()+mean)
+                banned_words_tensors.append(tokens[0].numpy())
+                banned_words_str.append(words)
                 number_of_words_to_ban -= 1
             elif res == 'n':
                 pass
             else:
                 print("incorrect value, try again please")
 
-def compare_words_chunks(inputs):
+
+number_of_good_strikes = 0
+number_of_miss_strikes = 0
+number_of_bad_strikes = 0
+nothing = 0
+counter = 100
+def compare_words_chunks(inputs,words):
+
+    global number_of_good_strikes
+    global number_of_miss_strikes
+    global number_of_bad_strikes
+    global nothing
+    global counter
+
     for i in range(0,len(banned_words_tensors)):
-        res = 1 - spatial.distance.cosine(inputs, banned_words_tensors[i])
-        print(res)
+        res = np.dot(inputs,banned_words_tensors[i]) / (np.linalg.norm(inputs) * np.linalg.norm(banned_words_tensors[i]))
+
         if res >= 0.5:
-            #make some noise
-            print("BAD WORD DETECTED !!!!!!")
+
+            if words in banned_words_str:
+                #make some noise
+                print("the value of the cosine similarity equals ",res)
+                print("BAD WORD DETECTED !")
+                number_of_good_strikes += 1
+                print("the number of good strike(s) equals ", number_of_good_strikes)
+                break
+
+            elif words not in banned_words_str:
+                print("the value of the cosine similarity equals ", res)
+                number_of_bad_strikes += 1
+                print("BAD WORD DETECTED but this word should not have been detected. The number of bad strike(s) equals ", number_of_bad_strikes)
+                break
+
         else:
-            print("all good")
+            if words in banned_words_str:
+                print("the value of the cosine similarity equals ", res)
+                number_of_miss_strikes += 1
+                print("This word should have been striked, the number of strikes that have been missed equals ",number_of_miss_strikes)
+                break
+            else:
+                print("all good")
+                nothing+=1
+                break
+
+    counter -= 1
+    print("the counter is equal to ", counter)
 
 with m as source:
-
-    print('You can speak now...')
+    #r.adjust_for_ambient_noise(source)
+    print('You can speak now...','\n')
     while True:
         audio = r.record(source,duration=1) #pyaudio object
         data = io.BytesIO(audio.get_wav_data()) #list of bytes
@@ -96,11 +106,12 @@ with m as source:
         words = str(tokenizer.batch_decode(tokens)[0]).lower()
 
         if words != '':
-            #var = numpy.var(tokens[0].numpy())
-            #compare_words_chunks((tokens[0].numpy()+numpy.mean(tokens[0].numpy()))/var)
-            compare_words_chunks(tokens[0].numpy()+numpy.mean(tokens[0].numpy()))
+            print('\n','Here\'s what has been captured : ', words)
+            #compare_words_chunks(tokens[0].numpy()+np.mean(tokens[0].numpy()),words)
+            compare_words_chunks(tokens[0].numpy(),words)
 
-            print('\n','Here\'s what has been captured : ',words)
-            #generated_text = generator(words, min_length=20)
-            #predictions = generator(words)[0]['generated_text']
-            #print("here's what's next : ",predictions)
+            if counter == 0:
+                print("End of the evaluation")
+                break
+
+print(number_of_good_strikes,number_of_miss_strikes,number_of_bad_strikes,nothing,sep='\n')
